f0a8af0484a2649bf8f5536d2b472ca155ebb19a
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-16 07:13:11 -0800

    updating metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,16 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
+	title: "ToDo",
+	description: "A simple todo app",
+	authors: {
+		name: "Takam Alex Christian"
+	}
+	
 };
 
 export default function RootLayout({
