300b0c0b5fca015bcac297360c2d2dc20cb89453
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-16 01:25:16 -0800

    the user is now informed when there are no todos

--- components/ToDoContainer.tsx
+++ components/ToDoContainer.tsx
@@ -12,7 +12,7 @@ function ToDoContianer() {
     const { state: toDoState } = useContext(toDoContext)
 
     return (
-        <div className="flex flex-col flex-grow gap-2">
+        <div className="flex flex-col gap-2  ">
             <AnimatePresence initial={true}>
 
 
@@ -24,6 +24,21 @@ function ToDoContianer() {
                     >
                         <ToDoItem toDo={eachToDo} key={index} />
                     </motion.div>)}
+
+                {toDoState.todos.length == 0 && 
+                    <motion.div
+                        initial={{opacity: 0}}
+                        animate= {{opacity: 1}}
+                        exit={{opacity: 0}}
+
+                        className="pos"
+                    >
+                        <div className="flex flex-col gap-2 absolute w-full h-[50%] py-12 items-center justify-center ">
+                            <h1 className="font-light text-4xl text-gray-400">You have no todos now</h1>
+                            <h3 className="font-medium text-base text-gray-400">Get Started by adding some ✌🏿</h3>
+                        </div>
+                    </motion.div>
+                }
             </AnimatePresence>
         </div>
     )
