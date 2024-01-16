d8e226aacf4aea134b58a9bf7af94910c12292fd
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-16 01:04:20 -0800

    display number of todo created with respect to the max

--- app/page.tsx
+++ app/page.tsx
@@ -8,41 +8,41 @@ import { InputBar } from "@/components/InputBar";
 
 import { useEffect, useReducer } from "react";
 
-import {ToDoState, ToDoReducerActions} from "@/types"
+import { ToDoState, ToDoReducerActions } from "@/types"
 
 export default function Home() {
 
 	// fetch local todo list
 	// put it to context
-	function updateStoredState(toDoState: ToDoState){
+	function updateStoredState(toDoState: ToDoState) {
 		if (toDoState.todos.length != 0) localStorage.setItem("toDoState", JSON.stringify(toDoState))
 	}
 
-	function toDoReducer(prevState: ToDoState, action: ToDoReducerActions ): ToDoState {
+	function toDoReducer(prevState: ToDoState, action: ToDoReducerActions): ToDoState {
 
-		switch (action.type){
-			case "added":  {
-				const newState = {...prevState, todos: [...prevState.todos, {...action.payload, id: Date.now().toString()}]}
-				
+		switch (action.type) {
+			case "added": {
+				const newState = { ...prevState, todos: [...prevState.todos, { ...action.payload, id: Date.now().toString() }] }
+
 				updateStoredState(newState)
 
 				return newState
 			}
 			case "completed": {
-				const foundToDo = prevState.todos.find(({id})=> id == action.payload.id? true : false)
+				const foundToDo = prevState.todos.find(({ id }) => id == action.payload.id ? true : false)
 
 				if (foundToDo) foundToDo.completed = true
-				
+
 				updateStoredState(prevState)
-				
+
 				return prevState
 			}
 			case "deleted": {
 
-				const newState = {todos: prevState.todos.filter(({id})=> id !== action.payload.id)}
+				const newState = { todos: prevState.todos.filter(({ id }) => id !== action.payload.id) }
 
 				updateStoredState(newState)
-				
+
 				return newState
 			}
 			case "initialized": {
@@ -60,22 +60,26 @@ export default function Home() {
 
 	const [toDoState, toDoDispatch] = useReducer(toDoReducer, initToDoState)
 
-	useEffect(()=>{
+	useEffect(() => {
 
 		const storedToDoState = localStorage.getItem("toDoState")
 
-		if (storedToDoState)  toDoDispatch({type: "initialized", payload: JSON.parse(storedToDoState)})
+		if (storedToDoState) toDoDispatch({ type: "initialized", payload: JSON.parse(storedToDoState) })
 
 	}, [])
 
 	return (
 		<toDoContext.Provider value={{ state: toDoState, dispatch: toDoDispatch }}>
 
-				<h1 className="pb-6 text-2xl font-light">ToDo</h1>
+			<div className="pb-6 flex flex-row justify-between items-center">
+				<h1 className="text-2xl font-light">ToDo</h1>
+				<h3 className="text-lg font-bold">{toDoState.todos.length}/8</h3>
+			</div>
 
-				<ToDoContianer />
 
-				<InputBar />
+			<ToDoContianer />
+
+			<InputBar />
 
 		</toDoContext.Provider>
 	);
